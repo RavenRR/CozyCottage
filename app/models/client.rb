0fefbe92deffa8d737cb3ebf5544d54a2e5489f7
romeroraven <romeroravenr@gmail.com>
2021-04-04 18:01:53 -0600

    Add model relationship

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ApplicationRecord
+  has_one_attached :image
   belongs_to :user
   belongs_to :category
+  has_many :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :appointments
+    has_many :clients
+    has_many :client_appointments, through: appointments, source: :client
 end
