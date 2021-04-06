9c5be34bf9fdb46d1ed970a97ea305a373408a98
romeroraven <romeroravenr@gmail.com>
2021-04-06 14:13:26 -0600

    Update Category Relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    has_many :appointments
-    has_many :users, through: :appointments
+    has_many :clients
+    has_many :users, through: :clients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :clients
-    has_many :categories, through: :appointments
+    has_many :categories, through: :clients
     has_many :appointments
     has_many :client_appointments, through: :appointments, source: :client
 end
