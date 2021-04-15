9343741ca80975bc3b30a17dfb392bf5846d72bc
romeroraven <romeroravenr@gmail.com>
2021-04-14 22:39:59 -0600

    Add CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+        #signup
+        def create
+            @user = User.new(user_params)
+            if @user.save
+                #log user in
+                session[:user_id] = @user.id
+                #redirect to show page
+                redirect_to @user
+            else
+                render :new
+            end
+        end
+        def show
+            @user = User.find_by_id(params[:id])
+            redirect_to '/' if !@user
+        end
+
+        private
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 end
