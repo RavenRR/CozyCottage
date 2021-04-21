6caa6c135e2c27331d433c769accf61449a184a5
romeroraven <romeroravenr@gmail.com>
2021-04-20 23:58:24 -0600

    Added Actions

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,30 @@
 class ClientsController < ApplicationController
+
+    def new
+        @client = Client.new
+    end
+
+    def create
+
+    end
+
+    def index
+        @clients = Client.all
+    end
+
+    def show 
+
+    end
+
+    def edit 
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
 end
