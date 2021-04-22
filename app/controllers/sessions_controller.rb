512477f308844d676cbd696a795f03060a7bac7e
romeroraven <romeroravenr@gmail.com>
2021-04-22 14:00:16 -0600

    Add user login session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
 
+    def new
+
+    end 
+
+    def create 
+        @user = User.find_by(email: params[:user][:email])
+
+        if @user && @user.authenticate(params[:user][:email])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            @error = "Invalid"
+            redirect_to login_path
+        end
+    end
+
+
+    def index
+
+    end
+
+
 end
