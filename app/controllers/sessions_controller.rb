2ad738627c6cb09f8d6279bf1c59413b3322dfc9
romeroraven <romeroravenr@gmail.com>
2021-04-26 22:45:24 -0600

    Update params/error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ class SessionsController < ApplicationController
     def create 
         @user = User.find_by(email: params[:user][:email])
 
-        if @user && @user.authenticate(params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            @error = "Invalid"
+            flash[:message] = "Invalid email or password, please try again"
             redirect_to login_path
         end
     end
