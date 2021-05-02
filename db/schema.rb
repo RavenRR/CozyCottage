540a7eaa62650027c2ac8a9f8f0ade4715874741
romeroraven <romeroravenr@gmail.com>
2021-05-02 17:39:30 -0600

    Add uid & provider to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_190642) do
+ActiveRecord::Schema.define(version: 2021_05_01_171226) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 2021_04_29_190642) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
