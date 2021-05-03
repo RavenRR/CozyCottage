6d8418e95d49fcfb2e80fd9d36481b4fe8366c49
romeroraven <romeroravenr@gmail.com>
2021-05-03 16:33:45 -0600

    Update method

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,6 @@
 class ClientsController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :set_client, only: [:show, :edit, :update, :destroy]
     def new
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
             @client = @user.clients.build
@@ -23,17 +24,15 @@ class ClientsController < ApplicationController
     end
 
     def show 
-        @client = Client.find_by_id(params[:id])
         redirect_to clients_path if !@client
     end
 
     def edit 
-        @client = Client.find_by_id(params[:id])
         redirect_to clients_path if !@client || @client.user != current_user
+        @client.build_category if !@client.category
     end
 
     def update
-        @client = Client.find_by_id(params[:id])
         redirect_to clients_path if !@client || @client.user != current_user
         if @client.update(client_params)
             redirect_to client_path(@client)
@@ -42,13 +41,18 @@ class ClientsController < ApplicationController
     end
 end
     def destroy
-        @client = Client.find_by_id(params[:id])
+        if !@client || @client.user != current_user
+            redirect_to clients_path
+        else
         @client.destroy
-
         redirect_to clients_path
+        end
     end
 end
 private
 def client_params
     params.require(:client).permit(:name, :image, :bio, :category_id, category_attributes: [:name])
+end
+    def set_client
+        @client = Client.find_by_id(params[:id])
 end
